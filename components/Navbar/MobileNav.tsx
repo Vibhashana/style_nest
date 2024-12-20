8b49ea30f140fc46f255dd5cc597cb0708157bf9
Vibhashana <svbbk.92@gmail.com>
2024-12-20 11:49:28 +0530

    Added Prettier configs, updated the nav

---
+++ components/Navbar/MobileNav.tsx
@@ -0,0 +1,65 @@
+import { Menu, X } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+
+interface MobileNavProps {
+  menu: { title: string; url: string }[];
+}
+
+const MobileNav = ({ menu }: MobileNavProps) => {
+  return (
+    <>
+      <Sheet>
+        <SheetTitle>
+          <span className="sr-only">Navigation menu panel</span>
+        </SheetTitle>
+        <SheetTrigger asChild>
+          <Button variant="ghost" size="icon" className="ml-4 md:hidden">
+            <Menu className="!size-6 text-neutral-500" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" showCloseButton={false}>
+          <div className="flex items-center justify-between">
+            <Link href="/" prefetch={false}>
+              <Image
+                src="/stylenest.svg"
+                alt="Style Nest"
+                width={105}
+                height={32}
+              />
+            </Link>
+            <SheetClose asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                aria-label="Close navigation menu"
+              >
+                <X className="text-neutral-500" />
+              </Button>
+            </SheetClose>
+          </div>
+          {menu.length > 0 && (
+            <div className="mt-8 grid gap-2">
+              {menu.map((item, index) => (
+                <Link key={index} href={item.url} className="text-neutral-500">
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
+
+export default MobileNav;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,67 +1,48 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { ShoppingBag, Menu, X } from "lucide-react"
-import { Sheet, SheetClose, SheetContent, SheetTitle, SheetTrigger } from "../ui/sheet"
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ShoppingBag } from "lucide-react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-    return (
-        <header className="">
-            <div className="content-container flex w-full items-center">
-                <nav className="flex w-full justify-between items-center py-4">
-                    <Link href="/" prefetch={false}>
-                        <Image src="/stylenest.svg" alt="Style Nest" width={105} height={32} />
-                    </Link>
-                    <div className="hidden md:flex gap-8 ml-20">
-                        <Link href="#" className="text-neutral-500">Shop all</Link>
-                        <Link href="#" className="text-neutral-500">Latest arrivals</Link>
-                    </div>
-                    <div className="ml-auto">
-                        <Button
-                            variant="ghost"
-                            size="icon"
-                            aria-label="Shopping cart"
-                        >
-                            <ShoppingBag className="!size-6 text-neutral-500" />
-                            <span className="sr-only">View shopping cart</span>
-                        </Button>
-                    </div>
-                </nav>
+  const menu = [
+    { title: "Shop all", url: "#" },
+    { title: "Latest arrivals", url: "#" },
+  ];
 
-                <Sheet>
-                    <SheetTitle>
-                        <span className="sr-only">Navigation menu panel</span>
-                    </SheetTitle>
-                    <SheetTrigger asChild>
-                        <Button variant="ghost" size="icon" className="md:hidden ml-4">
-                            <Menu className="!size-6 text-neutral-500" />
-                            <span className="sr-only">Toggle navigation menu</span>
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent side="left" showCloseButton={false}>
-                        <div className="flex justify-between items-center">
-                            <Link href="/" prefetch={false}>
-                                <Image src="/stylenest.svg" alt="Style Nest" width={105} height={32} />
-                            </Link>
-                            <SheetClose asChild>
-                                <Button 
-                                    variant="ghost" 
-                                    size="icon"
-                                    aria-label="Close navigation menu"
-                                >
-                                    <X className="text-neutral-500" />
-                                </Button>
-                            </SheetClose>
-                        </div>
-                        <div className="grid gap-2 mt-8">
-                            <Link href="#" className="text-neutral-500">Shop all</Link>
-                            <Link href="#" className="text-neutral-500">Latest arrivals</Link>
-                        </div>
-                    </SheetContent>
-                </Sheet>
+  return (
+    <header className="">
+      <div className="content-container flex w-full items-center">
+        <nav className="flex w-full items-center justify-between py-4">
+          <Link href="/" prefetch={false}>
+            <Image
+              src="/stylenest.svg"
+              alt="Style Nest"
+              width={105}
+              height={32}
+            />
+          </Link>
+          {menu.length > 0 && (
+            <div className="ml-20 hidden gap-8 md:flex">
+              {menu.map((item, index) => (
+                <Link key={index} href={item.url} className="text-neutral-500">
+                  {item.title}
+                </Link>
+              ))}
             </div>
-        </header>
-    )
-}
+          )}
 
-export default Navbar
+          <div className="ml-auto">
+            <Button variant="ghost" size="icon" aria-label="Shopping cart">
+              <ShoppingBag className="!size-6 text-neutral-500" />
+              <span className="sr-only">View shopping cart</span>
+            </Button>
+          </div>
+        </nav>
+        <MobileNav menu={menu} />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
